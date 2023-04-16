12d4cbbe654ebb1d1163b5c5eaed404e36c28380
Mikołaj Szczepek <m.szczepek01@gmail.com>
2023-04-16 16:45:25 +0200

    Create AccountController.cs

---
+++ Backend/CryptoMarket/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using CryptoMarket.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CryptoMarket.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<User> userManager;
+        private RoleManager<IdentityRole<Guid>> roleManager;
+        private SignInManager<User> signInManager;
+
+        public AccountController(UserManager<User> _userManager, RoleManager<IdentityRole<Guid>> _roleManager, SignInManager<User> _signInManager)
+        {
+            userManager = _userManager;
+            roleManager = _roleManager;
+            signInManager = _signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
