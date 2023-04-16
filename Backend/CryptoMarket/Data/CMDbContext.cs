c3a26c2f900f661921a65bf885e9cee28af5e59f
Mikołaj Szczepek <m.szczepek01@gmail.com>
2023-04-16 15:23:41 +0200

    Create CMDbContext.cs

---
+++ Backend/CryptoMarket/Data/CMDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace CryptoMarket.Data
+{
+    public class CMDbContext :DbContext
+    {
+        public CMDbContext(DbContextOptions<CMDbContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                    .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+                optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            }
+        }
+    }
+}
