18bd2eacbd62d64d2f52fe16330ec218a7c6d8cb
Mikołaj Szczepek <m.szczepek01@gmail.com>
2023-04-16 02:35:24 +0200

    [TEST] Connect BE with FE using API

--- Frontend/CryptoMarket/src/components/dashboard.vue
+++ Frontend/CryptoMarket/src/components/dashboard.vue
@@ -1,3 +1,35 @@
 <template>
+  <Navigation></Navigation>
 
+  <button label="Submit" icon="pi pi-check" icon-pos="right" @click="test" >test</button>
+  <div>{{ result }}</div>
 </template>
+
+<script lang="ts">
+  import { defineComponent, defineAsyncComponent, ref  } from 'vue';
+  import axios from 'axios';
+
+  const Navigation = defineAsyncComponent(() => import('./Navigation.vue'))
+
+  export default defineComponent({
+    components: {
+      Navigation
+    },
+    setup(){
+
+      const result = ref();
+      async function test() {
+        result.value = await axios.get('https://localhost:7139/WeatherForecast')
+      }
+
+      return{
+        test,
+        result
+      };
+    },
+  });
+</script>
+
+<style>
+@import '../styles/style.scss';
+</style>
