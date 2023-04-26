115b90e4ff4863e7185d19d0348cabcd528f0497
Mikołaj Szczepek <m.szczepek01@gmail.com>
2023-04-26 09:20:28 +0200

    Create UserRegistration.cs
    Add registration model

---
+++ Backend/CryptoMarket/BindingModels/UserRegistration.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CryptoMarket.BindingModels
+{
+    public class UserRegistration
+    {
+        [EmailAddress]
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+    }
+}
