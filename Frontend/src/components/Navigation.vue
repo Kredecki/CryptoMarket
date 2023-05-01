7657e117aa22a9e8d24d479108c2b889d2495734
Mikołaj Szczepek <m.szczepek01@gmail.com>
2023-05-01 21:47:29 +0200

    Simplify navi panel

--- Frontend/src/components/Navigation.vue
+++ Frontend/src/components/Navigation.vue
@@ -5,9 +5,95 @@
       <p class="market">Market</p>
     </div>
     <div id="navigation-center">
-      <ol >
-        <component v-for="n in arrLength" :is="components[arr[n]]"></component>
+      <ol>
+        <li><button>Buy Crypto<i class="arrow-down"></i></button></li>
+
+        
+          <li><button>Markets<i class="arrow-down"></i></button>
+            <ul>
+              <li>
+                <div id="navi-box0" class="navi-box">
+                        
+                </div>
+              </li>
+            </ul>
+          </li>
+        
+
+        <li><button>Trade<i class="arrow-down"></i></button>
+          <ul>
+            <li>
+              <div id="navi-box0" class="navi-box" :style="{ 'height': height + 'px'}">
+                
+                <div id="navi-square-border1" class="navi-square-border">
+                  <div id="navi-square0" class="navi-square">
+                    <div>
+                        <p>Spot Trading</p>
+                        <a>Buy & Sell on the Spot</a>
+                    </div>
+                    <ol>
+                        <li>
+                        <div id="navi-box00" class="navi-box" :style="{'background-color': '#393A3F', 'height': height + 'px'}">
+                            <div id="navi-square00" class="navi-square" style="background-color: #393A3F;">
+                            <div>
+                                <a id="selected" class="header01">USDT</a>
+                                <a class="header01">USDC</a>
+                                <a class="header01">BUSD</a>
+                                <a class="header01">BTC</a>
+                                <a class="header01">DAI</a>
+                                <a class="header01">EUR</a>
+                            </div>
+                            </div>
+                            <div id="navi-square01" class="navi-square" style="background-color: #393A3F;">
+                            <img class="coin" src="@/../public/btc.svg">
+                            <div>
+                                <p>BTC/USDT <img class="hot" src="@/../public/hot.svg"> </p>
+                                <a>Bitcoin</a>
+                            </div>
+                            </div>
+                        </div>
+                        </li>
+                    </ol>
+                  </div>     
+                </div>
+
+                <div id="navi-square-border2" class="navi-square-border">
+                  <div id="navi-square1" class="navi-square">
+                      <div>
+                      <p>Convert</p>
+                      <a>Quick conversion with zero fees and no slippage</a>
+                      </div>
+                  </div>
+                </div>
+
+                <div id="navi-square-border3" class="navi-square-border">
+                  <div id="navi-square2" class="navi-square">
+                      <div>
+                      <p>Margin Trading</p>
+                      <a>Invest with borrowed funds to boost returns</a>
+                      </div>
+                      <ol>
+                      <li>
+                          <div id="navi-box20" class="navi-box" :style="{'background-color': '#393A3F', 'height': height + 'px'}">
+                          <div id="navi-square20" class="navi-square" style="background-color: #393A3F;">
+                              <img class="coin" src="@/../public/btc.svg">
+                              <div>
+                              <p>BTC/USDT <img class="hot" src="@/../public/hot.svg"> </p>
+                              <a>Bitcoin</a>
+                              </div>
+                          </div>
+                          </div>
+                      </li>
+                      </ol>
+                  </div>
+                </div>
+                      
+              </div>
+            </li>
+          </ul>
+        </li>
       </ol>
+
     </div>
     <div id="navigation-right">
       <button class="LogInBtn" @click="sendView(NavigationItem.login)">Log In</button>
@@ -20,16 +106,10 @@
   import { defineComponent, defineAsyncComponent, ref } from 'vue';
   import { NavigationItem } from '../types/enums/NavigationEnum'
 
-  //Symulacja danych z BE
-  let arr: string[] = ['', 'Markets', 'Trade', 'BuyCrypto']
-  let arrLength = arr.length
+  let height = 270
 
   const components: {[key: string]: any} = {};
 
-  for(let x = 0; x < arrLength; x++){
-    components[arr[x]] = defineAsyncComponent(() => import(`./NavigationComponents/${arr[x]}.vue`))
-  }
-  
   export default defineComponent({
     components: {
       components
@@ -43,11 +123,10 @@
       }
 
       return{
-        arr,
         components,
-        arrLength,
         NavigationItem,
-        sendView
+        sendView,
+        height
       };
     },
   });
