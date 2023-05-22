832e1b2c9dfdb0637d5bc2f435b0d25ded28ab40
Mikołaj Szczepek <m.szczepek01@gmail.com>
2023-05-22 19:33:27 +0200

    Update SpotTrading.vue
    Delete chart and ticker

--- Frontend/src/components/SpotTrading.vue
+++ Frontend/src/components/SpotTrading.vue
@@ -1,53 +1,7 @@
 <template>
   <div style="background-color: #171B26;">
 
-    <Ticker class="STticker"
-      :options="{
-        symbols:[
-          {
-            proName: 'BITSTAMP:BTCUSD',
-            Title: 'Bitcoin'
-          },
-          {
-            proName: 'BITSTAMP:ETHUSD',
-            Title: 'Ethereum'
-          },
-          {
-            proName: 'CRYPTOCAP:XMR',
-            Title: 'Monero'
-          },
-          {
-            proName: 'BITGET:HEXUSDT',
-            Title: 'HEX'
-          },
-          {
-            proName: 'BINANCE:XRPUSDT',
-            Title: 'Ripple'
-          },
-        ],
-        colorTheme: 'dark',
-        isTransparent: false,
-        showSymbolLogo: true,
-        locale: 'en'
-      }"
-    />
-
     <div class="STcenter">
-
-      <Chart class="STchart"
-        :options="{
-          autosize: true,
-          symbol: 'BINANCE:BTCUSDT',
-          interval: 'D',
-          timezone: 'Etc/UTC',
-          theme: 'dark',
-          style: '1',
-          locale: 'en',
-          toolbar_bg: '#f1f3f6',
-          enable_publishing: false,
-          allow_symbol_change: true,
-        }"
-      />
 
       <div id="" class="STorder-book">
       </div>
@@ -94,13 +48,11 @@
 
 <script lang="ts">
   import { defineComponent, ref, onMounted } from 'vue';
-  import { Chart, Ticker } from 'vue-tradingview-widgets';
   import axios from 'axios';
 
   export default defineComponent({
     components: {
-      Chart,
-      Ticker
+
     },
     setup(){
 
